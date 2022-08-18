761a84083426d9234061e3945a1bae9397b3b7ed
Angelo Arcillas <angeloarcillas64@gmail.com>
2022-08-18 22:11:30 +0800

    Add multiple attribute on template

--- @vue/vue-docs/vue-docs-1/src/App.vue
+++ @vue/vue-docs/vue-docs-1/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import MultiAttributes from './components/MultiAttributes.vue';
 </script>
 
 <template>
@@ -13,7 +14,9 @@ import TheWelcome from './components/TheWelcome.vue'
   </header>
 
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+    <MultiAttributes />
+    
   </main>
 </template>
 
---
+++ @vue/vue-docs/vue-docs-1/src/components/MultiAttributes.vue
@@ -0,0 +1,18 @@
+<script setup>
+// !NOTE: don't overuse, decrease readability
+// defining multiple attribute on element
+const objectAttributes = {
+  id: 'my-id',
+  class: 'class-1 class-2',
+};
+</script>
+
+<template>
+  <p v-bind="objectAttributes">Hello Multiple Attributes</p>
+</template>
+
+<style scoped>
+.class-1 {
+  color: aqua;
+}
+</style>
