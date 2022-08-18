1c44345a2f2a1b036834c9ec11872b57c3cbd5c8
Angelo Arcillas <angeloarcillas64@gmail.com>
2022-08-18 22:25:49 +0800

    Add notes

--- @vue/vue-docs/vue-docs-1/src/components/ReactiveState.vue
+++ @vue/vue-docs/vue-docs-1/src/components/ReactiveState.vue
@@ -1,4 +1,5 @@
 <script setup>
+// !NOTE: JUST USE REF()
 import { reactive } from 'vue';
 
 const state = reactive({ count: 0 });
@@ -6,6 +7,27 @@ const state = reactive({ count: 0 });
 function increment() {
   state.count++;
 }
+
+
+// !NOTE: Advance stuffs
+// const books = reactive([ref('Vue 3 Guide')])
+// // need .value here
+// console.log(books[0].value)
+
+// const map = reactive(new Map([['count', ref(0)]]))
+// // need .value here
+// console.log(map.get('count').value)
+
+// // !NOTE: only use reactive when defining a state
+// const person = reactive({
+//   name: 'Albert',
+//   age: 30,
+//   isNinja: true,
+// });
+
+// const name = ref('Albert');
+// const age = ref(30);
+// const isNinja = ref(true);
 </script>
 
 <template>
