dda8a9456a4ef69471664caf47b5e3190deb9a46
Angelo Arcillas <69749146+zerexei@users.noreply.github.com>
2023-02-25 10:48:13 +0000

    router

--- snippets/js/HelloWorld.vue
+++ snippets/js/HelloWorld.vue
@@ -1,4 +1,5 @@
 <!-- 
+    !# stopped @ tuts 15
     TODO: research
     - axios.cancelToken 
 -->
@@ -33,7 +34,17 @@ const counter = counter_factory();
     </div>
 
     <HelloWorld />
-    <hello-world></
+    <hello-world></hello-world>
+
+    <router-link
+      :to="{ name: 'project', params: { project_id: 1, user_id: 1 } }"
+    ></router-link>
+
+    <transition></transition>
+    <transition-group></transition-group>
+    <keep-alive>
+      <!-- TODO: play with SPA tasks -->
+    </keep-alive>
   </div>
 </template>
 
@@ -44,8 +55,25 @@ export default {
     const api_key = 'qprcn1-2irnc12i4n-102op'; // not visible after setup lifecycle
     const public_key = '1c12-4n1p4c-n1p4';
 
+    watch(
+      // /:prop(<regex>) => /:UnFound(.*)* // 404 route
+      () => route.params.project_id,
+      (project_id, previous_project_id) => {
+        // watch route params project id
+        // execute actions
+        console.log(project_id);
+      }
+    );
+
     return {
       public_key, // make visible even after setup lifecycle
+    };
+  },
+  props: ['project_id'], // pass route params as props but not reactive +> props: true on route
+
+  props(route) {
+    return {
+      project_id: route.parmas.project_id, // reactive
     };
   },
   data() {
@@ -57,6 +85,18 @@ export default {
       return this.api_key; //not visible
       return this.public_key; // visisble
     },
+  },
+
+  // router lifecycle
+  beforeRouteEnter(to, from, next) {
+    next(() => {
+      // access component
+    });
+  },
+  beforeRouteUpdate(to, from) {},
+  beforeRouteLeave(to, from) {
+    const answer = window.confirm('Are you sure you want to leave?');
+    if (!answer) return false;
   },
 };
 </script>
